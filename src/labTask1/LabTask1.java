2d954e141810c8861f59265a7c91fa8fb3e627f5
Kshitij Varshney <kshitij.varshney_cs19@gla.ac.in>
2020-08-17 16:42:24 +0530

    create LabTask1.java

---
+++ src/labTask1/LabTask1.java
@@ -0,0 +1,16 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 24-Jul-20
+ *   Time: 9:54 AM
+ *   File: LabTask1.java
+ */
+
+package labTask1;
+
+
+//Task 1 - Create a "Hello, World!" program in Java.
+public class LabTask1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
