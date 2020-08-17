20eb3005d7e84e0f60328b285d33db59e017a3e4
Kshitij Varshney <kshitij.varshney_cs19@gla.ac.in>
2020-08-17 17:17:10 +0530

    creat 2nd lab class file

---
+++ src/labTask2/LabTask2.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 17-Aug-20
+ *   Time: 5:15 PM
+ *   File: LabTask2.java
+ */
+
+package labTask2;
+
+public class LabTask2 {
+}
---
+++ src/labTask2/LabTask3.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 17-Aug-20
+ *   Time: 5:16 PM
+ *   File: LabTask3.java
+ */
+
+package labTask2;
+
+public class LabTask3 {
+}
