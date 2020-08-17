c0dd48778b3283d56c744a96f045614f06be2ac7
Kshitij Varshney <kshitij.varshney_cs19@gla.ac.in>
2020-08-17 17:06:43 +0530

    add the code

--- src/labTask1/LabTask1.java
+++ src/labTask1/LabTask1.java
@@ -7,5 +7,10 @@
 
 package labTask1;
 
+//Task 1 - Create a "Hello, World!" program in Java
 public class LabTask1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
 }
