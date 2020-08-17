79943a72cca1a26949567d9b1a9615c8bc5cd0f9
Kshitij Varshney <kshitij.varshney_cs19@gla.ac.in>
2020-08-17 17:14:21 +0530

    create LabTask1.java

---
+++ src/labTask2/LabTask1.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 17-Aug-20
+ *   Time: 5:13 PM
+ *   File: LabTask1.java
+ */
+
+package labTask2;
+
+public class LabTask1 {
+}
