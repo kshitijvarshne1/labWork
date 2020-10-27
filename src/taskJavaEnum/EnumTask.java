1310436ee6d3664eab1ebd7083f8e0f7ec38a27d
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-27 18:55:12 +0530

    add fields and create getter method

--- src/taskJavaEnum/EnumTask.java
+++ src/taskJavaEnum/EnumTask.java
@@ -1,6 +1,27 @@
 package taskJavaEnum;
 enum months {
+    JANUARY(31),
+    FEBRUARY(28),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMBER(31);
 
+    private final int numberOfDays;
+
+    months(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+
+    public int getNumberOfDays() {
+        return numberOfDays;
+    }
 }
 
 public class EnumTask {
