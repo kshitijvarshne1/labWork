17c565f8ca79c432ba7f33a938835120fa3a39e5
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-29 19:10:00 +0530

    create main method

--- TheRabbitAndTheTortoiseRace/src/main/Race.java
+++ TheRabbitAndTheTortoiseRace/src/main/Race.java
@@ -1,4 +1,13 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 29-Oct-20
+ *   Time: 6:30 PM
+ *   File: Race.java
+ */
 package main;
 
 public class Race {
+    public static void main(String[] args) {
+
+    }
 }
