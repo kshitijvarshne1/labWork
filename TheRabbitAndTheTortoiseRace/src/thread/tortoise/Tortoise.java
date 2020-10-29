f3115525b59dca2429f842f1c14c23b93d3d2788
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-29 19:01:10 +0530

    class Tortoise.java extends Thread and override run method

--- TheRabbitAndTheTortoiseRace/src/thread/tortoise/Tortoise.java
+++ TheRabbitAndTheTortoiseRace/src/thread/tortoise/Tortoise.java
@@ -1,4 +1,20 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 29-Oct-20
+ *   Time: 6:37 PM
+ *   File: Tortoise.java
+ */
 package thread.tortoise;
 
-public class Tortoise {
+public class Tortoise extends Thread {
+    private static final int MILESTONES = 5;
+
+    public Tortoise() {
+        super("Tortoise");
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
