1650f1056e39a3c6e638d5ed87867e311ca0765e
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-29 19:08:57 +0530

    final commit

--- TheRabbitAndTheTortoiseRace/src/thread/rabbit/Rabbit.java
+++ TheRabbitAndTheTortoiseRace/src/thread/rabbit/Rabbit.java
@@ -7,19 +7,15 @@
 package thread.rabbit;
 
 import javax.swing.*;
-
 public class Rabbit implements Runnable {
     private static final int MILESTONES = 5;
     private final Thread rabbit;
-
     public Rabbit() {
         this.rabbit = new Thread(this, "Rabbit");
     }
-
     public Thread getRabbit() {
         return rabbit;
     }
-
     @Override
     public void run() {
         for (int index = 0; index < MILESTONES; index++) {
--- TheRabbitAndTheTortoiseRace/src/thread/tortoise/Tortoise.java
+++ TheRabbitAndTheTortoiseRace/src/thread/tortoise/Tortoise.java
@@ -10,30 +10,14 @@ import javax.swing.*;
 
 public class Tortoise extends Thread {
     private static final int MILESTONES = 5;
-
-    public Tortoise() {
-        super("Tortoise");
-    }
-
-    @Override
     public void run() {
         for (int index = 0; index < MILESTONES; index++) {
             switch (index) {
-                case 0:
-                    System.out.println("The Tortoise has started from the start line!");
-                    break;
-                case 1:
-                    System.out.println("The Tortoise has reached the Cool River!");
-                    break;
-                case 2:
-                    System.out.println("The Tortoise has reached the Mountain Hill!");
-                    break;
-                case 3:
-                    System.out.println("The Tortoise has reached the Big Oak Tree!");
-                    break;
-                case 4:
-                    System.out.println("The Tortoise has reached the finish line!");
-                    break;
+                case 0 -> System.out.println("The Tortoise has started from the start line!");
+                case 1 -> System.out.println("The Tortoise has reached the Cool River!");
+                case 2 -> System.out.println("The Tortoise has reached the Mountain Hill!");
+                case 3 -> System.out.println("The Tortoise has reached the Big Oak Tree!");
+                case 4 -> System.out.println("The Tortoise has reached the finish line!");
             }
             try {
                 Thread.sleep(3000L);
