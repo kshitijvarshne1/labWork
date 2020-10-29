6c2b8f3c27aa0d0d7ed01f5573589b5e3d12589d
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-29 18:50:02 +0530

    implements from Runnable class and create getter method and override the Rabbit.java

--- TheRabbitAndTheTortoiseRace/src/thread/rabbit/Rabbit.java
+++ TheRabbitAndTheTortoiseRace/src/thread/rabbit/Rabbit.java
@@ -1,4 +1,21 @@
 package thread.rabbit;
 
-public class Rabbit {
+public class Rabbit implements Runnable {
+
+    private static final int MILESTONES = 5;
+
+    private final Thread rabbit;
+
+    public Rabbit() {
+        this.rabbit = new Thread(this, "Rabbit");
+    }
+
+    public Thread getRabbit() {
+        return rabbit;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
